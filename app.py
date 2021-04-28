c9c3f15f3ade991ea5c2d196d01cd661cbeab917
Nikhil <181fa04465@gmail.com>
2021-04-28 10:36:54 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+app = Flask(__name__)
+model1 = pickle.load(open('finalized_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model1.predict(final_features)
+    output = round(prediction[0], 2)
+    if(output==0):
+        return render_template('index.html', prediction_text='no stroke')
+    else:
+        return render_template('index.html', prediction_text='stroke')
+
+if __name__ == "__main__":
+    app.run(debug=True)
